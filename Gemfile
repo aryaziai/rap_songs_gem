983d7ec2b5190026b2f02dc15c8b6e48e8f8a390
aryaziai <arya721@gmail.com>
2019-12-03 22:15:11 -0800

    update

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in rap_songs.gemspec
 gemspec
+
+# Testing
+gem 'rspec'
+
+# Debugging
+gem 'pry'
+
+gem 'require_all'
--- lib/rap_songs.rb
+++ lib/rap_songs.rb
@@ -1,9 +1,28 @@
-require "rap_songs/version"
+
+require_relative "../config/environment.rb"
+# require "rap_songs/version"
 
 module RapSongs
-  class Error < StandardError; end
- 
-  def hello
-    puts "hello there"
+ class Song
+
+  def random_song
+   ["IndigoChildRick - Vogue https://www.youtube.com/watch?v=ElOo0psAs3E",
+    "Divine Council - Dirtbags in Distress https://www.youtube.com/watch?v=u7Fw8OHnJZM",
+    "Sheck Wes - YKTS https://www.youtube.com/watch?v=mVFKMomXMOc",
+    "Yung Bans - Partna in Crime https://www.youtube.com/watch?v=v5ybdpttu40",
+    "Trippie Redd - Lil Wayne https://www.youtube.com/watch?v=pmdI9YdLQ_o",
+    ].sample
   end
+
+  def execute
+  puts "\n\nHold Command + Press Link:\n"
+  puts "#{random_song} \n\n\n"
+  end
+    
+
+  song = Song.new
+  song.execute
+
+
+ end
 end
